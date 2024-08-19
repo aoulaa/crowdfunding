796bf3155aa7b0f97ff2586866591fb65f261675
olim <o.nizomov@scalablesolutions.io>
2024-08-20 01:21:58 +0500

    project user models, migrations

---
+++ backend/apps/project/models.py
@@ -0,0 +1,99 @@
+from datetime import timedelta
+from django.db import models
+from django.utils import timezone
+from users.models import User
+
+choice = (
+    ('pending', 'Pending'),
+    ('approved', 'Approved'),
+    ('returned', 'Returned')
+)
+
+class Category(models.Model):
+    title = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return str(self.title)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return str(self.title)
+
+    class Meta:
+        verbose_name_plural = "Tags"
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=50)
+    details = models.TextField(max_length=2000)
+    target = models.PositiveIntegerField()
+    start_date = models.DateTimeField(default=timezone.now)
+    end_date = models.DateTimeField(default=timezone.now() + timedelta(days=14))
+    creation_date = models.DateTimeField(default=timezone.now)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, default=None, related_name="projects")
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    tags = models.ManyToManyField(Tag, related_name="projects")
+
+    def __str__(self):
+        return str(self.title)
+
+    class Meta:
+        verbose_name_plural = "Projects"
+
+class ProjectImage(models.Model):
+    path = models.ImageField(upload_to='project', verbose_name='Image')
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="images")
+
+    def __str__(self):
+        return str(self.path)
+
+    class Meta:
+        verbose_name_plural = "Project Images"
+
+
+
+
+class Investment(models.Model):
+    amount = models.PositiveIntegerField()
+    investment = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="investments")
+    investor = models.ForeignKey(User, on_delete=models.DO_NOTHING, default=None, related_name="investments")
+    status = models.CharField(max_length=10, choices=choice, default='pending')
+
+    def __str__(self):
+        return str(self.amount)
+
+    class Meta:
+        verbose_name_plural = "Investments"
+
+class Transaction(models.Model):
+    amount = models.PositiveIntegerField()
+    transaction_date = models.DateTimeField(default=timezone.now)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    investor = models.ForeignKey(User, on_delete=models.DO_NOTHING, default=None)
+
+    def __str__(self):
+        return str(self.amount)
+
+    class Meta:
+        verbose_name_plural = "Transactions"
+
+
+
+class RewardsEarned(models.Model):
+    investor = models.ForeignKey(User, on_delete=models.DO_NOTHING, default=None)
+    investment = models.ForeignKey(Investment, on_delete=models.DO_NOTHING, default=None)
+    amount = models.PositiveIntegerField()
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=2000)
+
+    def __str__(self):
+        return str(self.amount)
+
+    class Meta:
+        verbose_name_plural = "Rewards Earned"
--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -4,3 +4,7 @@ from django.db import models
 
 class User(AbstractUser):
     display_name = models.CharField(max_length=30, blank=True)
+    phone = models.CharField(max_length=15, verbose_name='phone', blank=True)
+    avatar = models.ImageField(upload_to="profile_images", default='profile_images/default.png', verbose_name='avatar')
+    country = models.CharField(max_length=20, blank=True, default='', verbose_name='country')
+    city = models.CharField(max_length=20, blank=True, default='', verbose_name='city')
