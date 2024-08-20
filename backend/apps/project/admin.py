d1097c6eb13035a4ab23e9649eb78c56ee3176a1
olim <o.nizomov@scalablesolutions.io>
2024-08-21 02:06:23 +0500

    end point for projects

--- backend/apps/project/admin.py
+++ backend/apps/project/admin.py
@@ -1,9 +1,39 @@
 from django.contrib import admin
 
-# from project.models import Project
-#
-#
-# @admin.register(Project)
-# class ProjectAdmin(admin.ModelAdmin):
-#     fields = ('id', 'title', 'is_active', 'user')
-#     readonly_fields = ['id', ]
+from project.models import Project, Investment, Transaction, RewardsEarned, Category, Tag
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    fields = (
+    'id', 'title', 'details', 'target', 'start_date', 'end_date', 'creation_date', 'creator', 'category', 'tags', 'is_deleted')
+    readonly_fields = ['id', ]
+
+
+@admin.register(Investment)
+class InvestmentAdmin(admin.ModelAdmin):
+    fields = ('id', 'amount', 'investor', 'project', 'status')
+    readonly_fields = ['id', ]
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    fields = ('id', 'amount', 'transaction_date', 'project', 'investor')
+    readonly_fields = ['id', ]
+
+
+@admin.register(RewardsEarned)
+class RewardsEarnedAdmin(admin.ModelAdmin):
+    fields = ('id', 'investor', 'investment', 'amount', 'title', 'description')
+    readonly_fields = ['id', ]
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    fields = ('id', 'title')
+    readonly_fields = ['id', ]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    fields = ('id', 'title')
+    readonly_fields = ['id', ]
